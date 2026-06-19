2ff6f3b9fee0d1d35999ff3d9a220705b9adff9b
admin <qtter85@gmail.com>
2026-06-19 23:40:54 +0300

    Разделение SSR и client

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,19 @@ import type { NextConfig } from "next";
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig: NextConfig = {
-  poweredByHeader: false,
-  compress: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-  experimental: {
-    optimizePackageImports: ["framer-motion", "radix-ui", "vaul"],
-  },
+    output: "standalone", // <-- Критически важно для легковесного Docker-образа
+    poweredByHeader: false,
+    compress: true,
+    compiler: {
+        removeConsole: process.env.NODE_ENV === "production",
+    },
+    experimental: {
+        optimizePackageImports: ["framer-motion", "radix-ui", "vaul"],
+    },
 };
 
 const withAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
+    enabled: process.env.ANALYZE === 'true',
 });
 
 export default withAnalyzer(nextConfig);
